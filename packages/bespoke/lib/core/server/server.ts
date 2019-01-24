be08f65058591c9ae6692cf34a9cd468027cc797
GuoKan <835111751@qq.com>
2019-01-24 14:36:09 +0800

    由game开进程启动server

--- packages/bespoke/lib/core/server/server.ts
+++ packages/bespoke/lib/core/server/server.ts
@@ -1,8 +1,6 @@
 'use strict'
 
 import * as path from 'path'
-import {AddressInfo} from 'net'
-import {createServer} from 'http'
 import {connect as connectMongo} from 'mongoose'
 import * as connectRedis from 'connect-redis'
 import {csrf} from 'lusca'
@@ -27,7 +25,7 @@ import {config} from '@common'
 import {EventDispatcher} from './controller/eventDispatcher'
 import requestRouter from './controller/requestRouter'
 import {serve as serveRPC} from './rpc'
-import {registerGameLogics} from '../../game/logic'
+import {AddressInfo} from 'net'
 
 const RedisStore = connectRedis(expressSession)
 
@@ -37,19 +35,10 @@ connectMongo(setting.mongoUri, {
     ...setting.mongoUser ? {user: setting.mongoUser, pass: setting.mongoPass} : {},
     useNewUrlParser: true,
     useCreateIndex: true
-} as {}).then(
-    ({connection}) => connection.on('error', () => {
-        console.error('MongoDB Connection Error. Please make sure MongoDB is running.')
-    })
-)
+}, err => err ? Log.e(err) : null)
 
-const port = (() => {
-    const _port = parseInt(process.env.PORT, 10)
-    return isNaN(_port) || _port <= 0 ? setting.port : _port
-})()
 const express = Express()
 WebpackHmr.applyHotDevMiddleware(express)
-express.set('views', path.join(__dirname, './view'))
 express.use(compression())
 express.use(morgan('dev'))
 express.use(bodyParser.json())
@@ -63,51 +52,44 @@ express.use(expressSession({
         client: redisClient as any
     })
 }))
-express.use((req, res, next) => {
-    csrf({
-        cookie: config.cookieKey.csrf
-    })(req, res, next)
-})
+express.use((req, res, next) => csrf({cookie: config.cookieKey.csrf})(req, res, next))
 express.use(passport.initialize())
 express.use(passport.session())
-express.use(function (req: any, res, next) {
+express.use((req, res, next) => {
     res.locals.user = req.user
     next()
 })
 express.use(errorHandler())
-express.use(`/${config.rootName}/static`, Express.static(
-    path.join(__dirname, '../../../dist/'),
-    {maxAge: '10d'}
-))
+express.use(`/${config.rootName}/static`, Express.static(path.join(__dirname, '../../../dist/'), {maxAge: '10d'}))
 express.use(`/${config.rootName}`, requestRouter)
-express.set('port', port)
-export const server = createServer(express)
-server.listen(port)
-    .on('error', (error: NodeJS.ErrnoException) => {
-        if (error.syscall !== 'listen') {
-            throw error
-        }
-        const bind = `${typeof port !== 'number' ? 'Pipe' : 'Port'} ${port}`
-        switch (error.code) {
-            case 'EACCES':
-                console.error(bind + ' requires elevated privileges')
-                process.exit(1)
-                break
-            case 'EADDRINUSE':
-                console.error(bind + ' is already in use')
-                process.exit(1)
-                break
-            default:
-                throw error
-        }
-    })
-    .on('listening', () => {
-        const address = server.address()
-        Log.i(`Listening on ${typeof address === 'string' ? `pipe ${address}` : `port ${(<AddressInfo>address).port}`}`)
-    })
-EventDispatcher.startGameSocket(server)
-registerGameLogics()
 
-if(!config.deployIndependently){
-    serveRPC()
+export function startServer(port: number = 0): Express.Express {
+    const server = express.listen(port)
+        .on('error', (error: NodeJS.ErrnoException) => {
+            if (error.syscall !== 'listen') {
+                throw error
+            }
+            const bind = `${typeof port !== 'number' ? 'Pipe' : 'Port'} ${port}`
+            switch (error.code) {
+                case 'EACCES':
+                    console.error(bind + ' requires elevated privileges')
+                    process.exit(1)
+                    break
+                case 'EADDRINUSE':
+                    console.error(bind + ' is already in use')
+                    process.exit(1)
+                    break
+                default:
+                    throw error
+            }
+        })
+        .on('listening', () => {
+            const address = server.address()
+            Log.i(`Listening on ${typeof address === 'string' ? `pipe ${address}` : `port ${(<AddressInfo>address).port}`}`)
+        })
+    EventDispatcher.startGameSocket(server)
+    if (!config.deployIndependently) {
+        serveRPC()
+    }
+    return express
 }
