17c087754e7e0cc4167bc5e4c03e2c8d1178d5f7
GuoKan <835111751@qq.com>
2019-02-13 17:58:23 +0800

    CognitiveReaction

--- packages/bespoke/game/CognitiveReaction/src/config.ts
+++ packages/bespoke/game/CognitiveReaction/src/config.ts
@@ -1,3 +1,5 @@
+export const namespace = 'CognitiveReaction'
+
 export enum MoveType {
     //player
     submitSeatNumber = 'submitSeatNumber',
---
+++ packages/bespoke/game/CognitiveReaction/src/setting.sample.ts
@@ -0,0 +1,8 @@
+export const setting = {
+    port: 3009,
+    rpcPort: 53009,
+    proxyService:{
+        host: '127.0.0.1',
+        port: 58888
+    }
+}
