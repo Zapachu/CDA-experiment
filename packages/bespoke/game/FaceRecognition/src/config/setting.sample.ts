0f94076685f27e1c09da9dc060527a8805510ddf
GuoKan <835111751@qq.com>
2019-02-13 17:38:02 +0800

    FaceRecognition

--- packages/bespoke/game/FaceRecognition/src/config/setting.sample.ts
+++ packages/bespoke/game/FaceRecognition/src/config/setting.sample.ts
@@ -1,11 +1,15 @@
-import {qiNiu} from '../../../core/src/client/script/setting'
+import {qiNiu} from '@dev/client/script/setting'
 
 export default {
+    port: 3009,
+    rpcPort: 53009,
+    proxyService:{
+        host: '127.0.0.1',
+        port: 58888
+    },
     qiNiu,
     ocpApim: {
         gateWay: 'https://api.cognitive.azure.cn/face/v1.0/detect',
         subscriptionKey: '4210--------------------'
     }
 }
-
-
---
+++ packages/bespoke/game/FaceRecognition/src/serve.ts
@@ -0,0 +1,15 @@
+require('../../../registerTsconfig')
+import {Server} from '@dev/server'
+import {namespace} from './config'
+import Controller from './Controller'
+import * as path from 'path'
+import setting from './config/setting'
+
+Server.start({
+    namespace,
+    port: +process.env.PORT || setting.port,
+    rpcPort: +process.env.RPC_PORT || setting.rpcPort,
+    proxyService: setting.proxyService,
+    staticPath: path.resolve(__dirname, '../dist'),
+    getClientPath: () => require(`../dist/${namespace}.json`)[`${namespace}.js`]
+}, {Controller})
