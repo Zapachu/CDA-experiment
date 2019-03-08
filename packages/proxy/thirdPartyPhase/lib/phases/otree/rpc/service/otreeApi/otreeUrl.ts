d4bbf97a69d963f85355cfd3e0e0927178a986d5
shinepans <shinepans@live.com>
2019-03-08 15:19:35 +0800

    otree

--- packages/proxy/thirdPartyPhase/lib/phases/otree/rpc/service/otreeApi/otreeUrl.ts
+++ packages/proxy/thirdPartyPhase/lib/phases/otree/rpc/service/otreeApi/otreeUrl.ts
@@ -10,6 +10,12 @@ const localOtreeRootUrl = settings.localOtreeRootUrl
 import * as rp from 'request-promise'
 import ListMap from '../../../server/utils/ListMap'
 
+declare interface IGetUrlByNamespace {
+    err: number,
+    phaseId: string,
+    playUrl: string
+}
+
 const gen32Token = (source) => {
     return objectHash(source, {algorithm: 'md5'})
 }
@@ -33,43 +39,51 @@ const getDemoList = async (namespace) => {
 }
 
 // get play link
-const getUrlByNamespace = async (groupId: string, namespace: string, param: string, owner: string) => {
-    const playHashs = []
-    const paramJson = JSON.parse(param)
-    const ownerToken = gen32Token(owner.toString())
-    const otreeDemoClickUrl = `${otreeDemoUrlPrefix}${paramJson.otreeName}`
-    console.log('req')
-    const options = {method: 'GET', uri: otreeDemoClickUrl, resolveWithFullResponse: true}
-    const res = await rp(options)
-    let body = res.body
-    let content = body.split(otreeParticipantUrl)
-    delete content[0]
-    content.map(con => {
-        if (playHashs.indexOf(con.slice(0, 8)) == -1) {
-            playHashs.push(con.slice(0, 8))
-        }
+const getUrlByNamespace = async (groupId: string, namespace: string, param: string, owner: string): Promise<IGetUrlByNamespace> => {
+    return new Promise<IGetUrlByNamespace>((resolve, reject) => {
+        const playHashs = []
+        const paramJson = JSON.parse(param)
+        const ownerToken = gen32Token(owner.toString())
+        const otreeDemoClickUrl = `${otreeDemoUrlPrefix}${paramJson.otreeName}`
+        console.log('req')
+        const options = {method: 'GET', uri: otreeDemoClickUrl, resolveWithFullResponse: true}
+        rp(options).then(async res => {
+            let body = res.body
+            let content = body.split(otreeParticipantUrl)
+            delete content[0]
+            content.map(con => {
+                if (playHashs.indexOf(con.slice(0, 8)) == -1) {
+                    playHashs.push(con.slice(0, 8))
+                }
+            })
+            const playHashsConf: { hash: string, player: string }[] = []
+            // 预设玩家 hash， 皆为未分配
+            playHashs.map(hash => {
+                playHashsConf.push({hash: hash, player: 'wait'})
+            })
+            const adminUrl = res.request.uri.path
+            const newOtreePhase = await new ThirdPartPhase({
+                param: param,
+                groupId: groupId,
+                namespace: namespace,
+                playHashs: playHashsConf,
+                adminUrl: adminUrl,
+                prefixUrl: otreePlayUrl,
+                ownerToken: ownerToken,
+            }).save()
+
+            const phaseId = newOtreePhase._id.toString()
+            console.log(`initialPlayUrl: ${localOtreeRootUrl}/init/${otreeParticipantUrl}${phaseId}`)
+
+            resolve({
+                err: 0,
+                phaseId: newOtreePhase._id.toString(),
+                playUrl: `${localOtreeRootUrl}/init/${otreeParticipantUrl}${phaseId}`
+            })
+        }).catch(err => {
+            console.log(err)
+        })
     })
-    const playHashsConf: { hash: string, player: string }[] = []
-    playHashs.map(hash => playHashsConf.push({hash: hash, player: 'wait'}))
-    const adminUrl = res.request.uri.path
-    const newOtreePhase = await new ThirdPartPhase({
-        param: param,
-        groupId: groupId,
-        namespace: namespace,
-        playHashs: playHashsConf,
-        adminUrl: adminUrl,
-        prefixUrl: otreePlayUrl,
-        ownerToken: ownerToken,
-    }).save()
-
-    const phaseId = newOtreePhase._id.toString()
-    console.log(`initialPlayUrl: ${localOtreeRootUrl}/init/${otreeParticipantUrl}${phaseId}`)
-
-    return {
-        err: 0,
-        phaseId: newOtreePhase._id.toString(),
-        playUrl: `${localOtreeRootUrl}/init/${otreeParticipantUrl}${phaseId}`
-    }
 }
 
 export {getUrlByNamespace, getDemoList}
