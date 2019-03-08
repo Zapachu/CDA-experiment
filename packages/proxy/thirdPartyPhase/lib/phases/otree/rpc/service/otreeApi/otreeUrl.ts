f18e048ebab658698b1e7cc41633035c30b359b2
shinepans <shinepans@live.com>
2019-03-08 17:58:43 +0800

    add sleep in req and rereq until session created

--- packages/proxy/thirdPartyPhase/lib/phases/otree/rpc/service/otreeApi/otreeUrl.ts
+++ packages/proxy/thirdPartyPhase/lib/phases/otree/rpc/service/otreeApi/otreeUrl.ts
@@ -8,6 +8,7 @@ const otreeParticipantUrl = 'InitializeParticipant/'
 const localOtreeRootUrl = settings.localOtreeRootUrl
 
 import * as rp from 'request-promise'
+import {Response} from 'express'
 import ListMap from '../../../server/utils/ListMap'
 
 declare interface IGetUrlByNamespace {
@@ -39,51 +40,68 @@ const getDemoList = async (namespace) => {
 }
 
 // get play link
-const getUrlByNamespace = async (groupId: string, namespace: string, param: string, owner: string): Promise<IGetUrlByNamespace> => {
-    return new Promise<IGetUrlByNamespace>((resolve, reject) => {
-        const playHashs = []
-        const paramJson = JSON.parse(param)
-        const ownerToken = gen32Token(owner.toString())
-        const otreeDemoClickUrl = `${otreeDemoUrlPrefix}${paramJson.otreeName}`
-        console.log('req')
-        const options = {method: 'GET', uri: otreeDemoClickUrl, resolveWithFullResponse: true}
-        rp(options).then(async res => {
-            let body = res.body
-            let content = body.split(otreeParticipantUrl)
-            delete content[0]
-            content.map(con => {
-                if (playHashs.indexOf(con.slice(0, 8)) == -1) {
-                    playHashs.push(con.slice(0, 8))
-                }
-            })
-            const playHashsConf: { hash: string, player: string }[] = []
-            // 预设玩家 hash， 皆为未分配
-            playHashs.map(hash => {
-                playHashsConf.push({hash: hash, player: 'wait'})
-            })
-            const adminUrl = res.request.uri.path
-            const newOtreePhase = await new ThirdPartPhase({
-                param: param,
-                groupId: groupId,
-                namespace: namespace,
-                playHashs: playHashsConf,
-                adminUrl: adminUrl,
-                prefixUrl: otreePlayUrl,
-                ownerToken: ownerToken,
-            }).save()
+const getUrlByNamespace = async (groupId, namespace, param, owner):Promise<any> => {
+    let inited = false, doneResBody = '', handleBody = ''
+    const syncWaitingForCreated= async (uri) => {
+        while (!inited) {
+            const options = {method: 'GET', uri, resolveWithFullResponse: true}
+            const doneRes = await rp(options)
+            console.log(doneRes.toJSON())
+            if (doneRes.body.toString().indexOf(otreeParticipantUrl) !== -1) {
+                doneResBody = doneRes.body
+                inited = true
+            }
+            await sleep(1000)
+        }
+        return doneResBody
+    }
+    const sleep = (ms) => {
+        return new Promise(resolve => setTimeout(resolve, ms))
+    }
 
-            const phaseId = newOtreePhase._id.toString()
-            console.log(`initialPlayUrl: ${localOtreeRootUrl}/init/${otreeParticipantUrl}${phaseId}`)
-
-            resolve({
-                err: 0,
-                phaseId: newOtreePhase._id.toString(),
-                playUrl: `${localOtreeRootUrl}/init/${otreeParticipantUrl}${phaseId}`
-            })
-        }).catch(err => {
-            console.log(err)
-        })
+    const playHashs = []
+    const paramJson = JSON.parse(param)
+    const ownerToken = gen32Token(owner.toString())
+    const otreeDemoClickUrl = `${otreeDemoUrlPrefix}${paramJson.otreeName}`
+    const initOptions = {method: 'GET', uri: otreeDemoClickUrl, resolveWithFullResponse: true}
+    const initRes = await rp(initOptions)
+    const waitingUrl = initRes.request.href
+    console.log(waitingUrl)
+    console.log(initRes.toJSON())
+    if (waitingUrl.toString().indexOf('WaitUntilSessionCreated/') !== -1) {
+        handleBody = await syncWaitingForCreated(waitingUrl)
+    }
+    let content = handleBody.split(otreeParticipantUrl)
+    console.log(content)
+    delete content[0]
+    content.map(con => {
+        if (playHashs.indexOf(con.slice(0, 8)) == -1) {
+            playHashs.push(con.slice(0, 8))
+        }
     })
+    const playHashsConf: { hash: string, player: string }[] = []
+    // 预设玩家 hash， 皆为未分配
+    playHashs.map(hash => {
+        playHashsConf.push({hash: hash, player: 'wait'})
+    })
+    const adminUrl = initRes.request.uri.path
+    const newOtreePhase = await new ThirdPartPhase({
+        param: param,
+        groupId: groupId,
+        namespace: namespace,
+        playHashs: playHashsConf,
+        adminUrl: adminUrl,
+        prefixUrl: otreePlayUrl,
+        ownerToken: ownerToken,
+    }).save()
+
+    const phaseId = newOtreePhase._id.toString()
+    console.log(`initialPlayUrl: ${localOtreeRootUrl}/init/${otreeParticipantUrl}${phaseId}`)
+    return {
+        err: 0,
+        phaseId: newOtreePhase._id.toString(),
+        playUrl: `${localOtreeRootUrl}/init/${otreeParticipantUrl}${phaseId}`
+    }
 }
 
 export {getUrlByNamespace, getDemoList}
