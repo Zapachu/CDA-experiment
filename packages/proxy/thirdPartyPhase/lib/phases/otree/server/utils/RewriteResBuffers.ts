2b55e15bc29573e9818e5a683399125affe1ada0
shinepans <shinepans@live.com>
2019-03-29 10:49:06 +0800

    get screen info and insert to first page

--- packages/proxy/thirdPartyPhase/lib/phases/otree/server/utils/RewriteResBuffers.ts
+++ packages/proxy/thirdPartyPhase/lib/phases/otree/server/utils/RewriteResBuffers.ts
@@ -1,15 +1,58 @@
 'use strict'
 
 
+const {Gzip} = require('zlib')
+import * as zlib from "zlib"
 import {ThirdPartPhase} from "../../../../core/server/models"
+import {generateInsertScript} from './generateInsertScript'
 import {gameService} from "../../../common/utils"
 import {elfSetting as settings} from 'elf-setting'
 const {oTreeProxy} = settings
 
+const GET_METHOD = 'GET'
+const START_SIGN = 'InitializeParticipant'
+const END_SIGN = 'OutOfRangeNotification'
+
+
 export const rewriteResBuffers = async (proxyRes, req, res) => {
 
-    const isEnd = req.url.includes('OutOfRangeNotification')  // 是否结束标志
-    const otreeParticipantUrl = 'InitializeParticipant/'      // 初始化的标志
+    const isGet = req.method === GET_METHOD
+    const isEnd = req.url.includes(END_SIGN)
+    const isStart = req.url.includes(START_SIGN)
+
+    if (isGet && isStart) {
+        console.log(req.session.oTreePhaseId)
+        const originWrite = res.write
+        const originEnd = res.end
+        let gzipStream = Gzip()
+        let buffers = []
+        res.write = (chunk) => {
+            buffers.push(chunk)
+            return true
+        }
+        res.end = () => {
+            let fullChunk = Buffer.concat(buffers)
+            zlib.unzip(fullChunk, {finishFlush: zlib.constants.Z_SYNC_FLUSH}, async (err, buffer) => {
+                if (!err) {
+                    gzipStream.on('data', chunk => {
+                        originWrite.call(res, chunk)
+                        gzipStream.end()
+                    })
+                    gzipStream.on('end', () => {
+                        originEnd.call(res)
+                    })
+                    const data = buffer.toString()
+                    const dataStart = data.split('<head>')[0]
+                    const dataEnd = data.split('<head>')[1]
+                    const newData = dataStart + '<head>' + generateInsertScript() + dataEnd
+                    const removeSafePart = newData.replace('<script src="//js.aq.qq.com/js/aq_common.js"></script>', '')
+                    gzipStream.write(new Buffer(removeSafePart))
+                } else {
+                    console.log(err)
+                }
+            })
+        }
+    }
 
     if (isEnd) {
         const originWrite = res.write
@@ -20,7 +63,7 @@ export const rewriteResBuffers = async (proxyRes, req, res) => {
             res.end = originEnd
             return res
         }
-        res.write = (r) => {}
+        res.write = () => {}
         res.end = () => {}
         let playerGameHash: string
         const playerOtreeHash: string = req.headers.referer.split('/p/')[1].split('/')[0]
@@ -37,7 +80,7 @@ export const rewriteResBuffers = async (proxyRes, req, res) => {
             })
             const params: { nextPhaseKey: string } = JSON.parse(otreePhase.param)
             const groupId: string = otreePhase.groupId
-            const playUrl: string = `${oTreeProxy}/init/${otreeParticipantUrl}${otreePhase._id}`
+            const playUrl: string = `${oTreeProxy}/init/${START_SIGN}/${otreePhase._id}`
             const playerToken: string = playerGameHash
             const nextPhaseKey: string = params.nextPhaseKey
             gameService.sendBackPlayer({
---
+++ packages/proxy/thirdPartyPhase/lib/phases/otree/server/utils/generateInsertScript.ts
@@ -0,0 +1,43 @@
+'use strict'
+
+const generateInsertScript = () => {
+    return `
+    
+    <script>
+        (function () {
+            var winW = 0, winH = 0;
+            if (document.body && document.body.offsetWidth) {
+                winW = document.body.offsetWidth;
+                winH = document.body.offsetHeight;
+            }
+            if (document.compatMode=='CSS1Compat' &&
+                document.documentElement &&
+                document.documentElement.offsetWidth ) {
+                winW = document.documentElement.offsetWidth;
+                winH = document.documentElement.offsetHeight;
+            }
+            if (window.innerWidth && window.innerHeight) {
+                winW = window.innerWidth;
+                winH = window.innerHeight;
+            }
+            console.log(winW, winH);
+            var myHeaders = new Headers();
+            var init = {
+                method: 'POST',
+                headers: myHeaders,
+                mode: 'cors',
+                cache: 'default' 
+            };
+            fetch('/report/screen', init).then(function (log) {
+                console.log(log);
+            }).catch(function (err) {
+                console.log(err);
+            });
+        })()
+    </script>
+    `
+}
+
+export {
+    generateInsertScript
+}
