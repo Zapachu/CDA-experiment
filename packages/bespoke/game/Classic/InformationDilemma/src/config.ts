a9971e67ec6edde77f1336e85d7f2391321f7a84
shinepans <shinepans@live.com>
2019-04-08 17:24:45 +0800

    信息困境与信息层叠 Tested

---
+++ packages/bespoke/game/Classic/InformationDilemma/src/config.ts
@@ -0,0 +1,29 @@
+export const namespace = 'InformationDilemma'
+
+export enum MoveType {
+    shout = 'shout',
+    getPosition = 'getPosition',
+}
+
+export enum PushType {
+    newRoundTimer
+}
+
+export enum FetchType {
+
+}
+
+export enum PlayerStatus {
+    prepared,
+    timeToShout,
+    shouted,
+    gameOver
+}
+
+export enum Balls {
+    red,
+    blue
+}
+
+
+export const NEW_ROUND_TIMER = 3
