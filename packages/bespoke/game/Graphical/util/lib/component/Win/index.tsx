42b7128b0e094eb946198466309f8135036b308b
GuoKan <835111751@qq.com>
2019-04-11 15:01:29 +0800

    Graphical组件提取

---
+++ packages/bespoke/game/Graphical/util/lib/component/Win/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import {span} from '../Stage'
+
+export function Win() {
+    return  <image
+        width={span(1.5)}
+        href={require('./win.gif')}/>
+}
--- packages/bespoke/game/Graphical/util/lib/index.ts
+++ packages/bespoke/game/Graphical/util/lib/index.ts
@@ -1,1 +1,1 @@
-export * from './imgGroup'
+export * from './component'
