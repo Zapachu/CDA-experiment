fbac837bfb57dfc7e26be1900a7c49fd7cf77245
GuoKan <835111751@qq.com>
2019-04-25 11:54:13 +0800

    自定义bespoke-game日志路径

--- packages/bespoke/core/server/src/backend/util/util.ts
+++ packages/bespoke/core/server/src/backend/util/util.ts
@@ -5,21 +5,6 @@ import {resolve} from 'path'
 import {readFileSync} from 'fs'
 import * as objHash from 'object-hash'
 
-const LogPath = resolve(__dirname, '../../../../../log')
-
-const {log: l, info: i, debug: d, warn: w, error: e} = (elfSetting.inProductEnv ? dailyfile : colorConsole)({
-    level: config.logLevel.toString(),
-    root: LogPath
-} as any)
-
-export const Log = {l, d, i, w, e}
-
-if (elfSetting.inProductEnv) {
-    console.log(`当前为生成环境,日志记录于:${LogPath}`)
-} else {
-    Log.w('当前为开发环境,短信/邮件发送、游戏状态持久化等可能受影响')
-}
-
 export class Hash {
     static hashObj(obj: any): string {
         return objHash(obj, {algorithm: 'md5'})
@@ -35,6 +20,28 @@ export function elfPhaseId2PlayUrl(namespace: string, phaseId: string): string {
     return `${host.startsWith('http') ? host : `http://${host}:${port}`}/${config.rootName}/${namespace}/play/${phaseId}`
 }
 
+export namespace Log {
+    let logger = colorConsole()
+
+    export const l = (...args) => logger.log(...args)
+    export const i = (...args) => logger.info(...args)
+    export const d = (...args) => logger.debug(...args)
+    export const w = (...args) => logger.warn(...args)
+    export const e = (...args) => logger.error(...args)
+
+    export function init(logPath: string) {
+        if (elfSetting.inProductEnv) {
+            logger = dailyfile({
+                level: config.logLevel.toString(),
+                root: logPath
+            })
+            console.log(`当前为生成环境,日志记录于:${logPath}`)
+        } else {
+            this.l('当前为开发环境,短信/邮件发送、游戏状态持久化等可能受影响')
+        }
+    }
+}
+
 export class Setting {
     static staticPath: string
     private static _port: number
@@ -48,12 +55,12 @@ export class Setting {
         return this._rpcPort
     }
 
-    static setPort(port:number){
+    static setPort(port: number) {
         this._port = port
         Log.i(`Listening on port ${port}`)
     }
 
-    static setRpcPort(rpcPort:number){
+    static setRpcPort(rpcPort: number) {
         this._rpcPort = rpcPort
         Log.i(`Serving on rpcPort ${rpcPort}`)
     }
@@ -62,6 +69,7 @@ export class Setting {
         this.staticPath = setting.staticPath
         this._port = setting.port || (elfSetting.inProductEnv ? 0 : config.devPort.server)
         this._rpcPort = setting.rpcPort || 0
+        Log.init(setting.logPath || resolve(setting.staticPath, '../log'))
     }
 
     static getClientPath(): string {
