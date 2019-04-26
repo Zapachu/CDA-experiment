4e5dafb8525675340fe6e1acfdf0082fd3e56b21
shinepans <shinepans@live.com>
2019-04-26 11:02:42 +0800

    add CreateOnElf Surpport, 3 graphical, 6 classic added

--- packages/bespoke/game/Classic/CommonValueAuction/src/view/index.tsx
+++ packages/bespoke/game/Classic/CommonValueAuction/src/view/index.tsx
@@ -2,10 +2,12 @@ import {registerOnFramework} from 'bespoke-client-util'
 import {Create} from './Create'
 import {Play} from './Play'
 import {Result} from './Result'
+import {CreateOnElf} from './CreateOnElf'
 
 registerOnFramework('CommonValueAuction', {
     localeNames: ['公共价值拍卖', 'CommonValueAuction'],
     Create,
+    CreateOnElf,
     Play,
     Result,
 })
--- packages/bespoke/game/Graphical/TheTragedyOfTheCommonsGraphical/src/view/index.tsx
+++ packages/bespoke/game/Graphical/TheTragedyOfTheCommonsGraphical/src/view/index.tsx
@@ -2,10 +2,12 @@ import {registerOnFramework} from 'bespoke-client-util'
 import {Create} from './Create'
 import {Play} from './Play'
 import {Result} from './Result'
+import {CreateOnElf} from './CreateOnElf'
 
 registerOnFramework('TheTragedyOfTheCommonsGraphical', {
     localeNames: ['公地悲剧动画版', 'The Tragedy Of The Commons (Graphical)'],
     Create,
+    CreateOnElf,
     Play,
     Result,
 })
