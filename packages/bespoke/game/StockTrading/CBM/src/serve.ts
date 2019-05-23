b9a37dfe772a9a05fd1befe2759f60545e71ff4d
GuoKan <835111751@qq.com>
2019-05-23 14:37:55 +0800

    【StockTrade】创建实验接口

--- packages/bespoke/game/StockTrading/CBM/src/serve.ts
+++ packages/bespoke/game/StockTrading/CBM/src/serve.ts
@@ -1,10 +1,24 @@
 import {resolve} from 'path'
 import {namespace} from './config'
-import {Server} from 'bespoke-server'
+import {Server, RedisCall, gameId2PlayUrl} from 'bespoke-server'
 import Controller from './Controller'
 import Robot from './Robot'
+import {ICreateParams} from './config'
+import {CreateGame, Phase} from '../../protocol'
 
 Server.start(
     {namespace, staticPath: resolve(__dirname, '../dist')},
     {Controller, Robot}
 )
+
+RedisCall.handle<CreateGame.IReq, CreateGame.IRes>(CreateGame.name(Phase.CBM), async ({keys}) => {
+    const gameId = await Server.newGame<ICreateParams>(namespace, {
+        title: `${Phase.CBM}:${Math.random()}`,
+        desc: '',
+        params: {
+            prepareTime: 15,
+            tradeTime: 180
+        }
+    })
+    return {playUrls: keys.map(key => gameId2PlayUrl(namespace, gameId, key))}
+})
