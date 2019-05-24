d8909b38f01cadf2301a4579a54deda8d1e4dbb1
GuoKan <835111751@qq.com>
2019-05-24 16:32:26 +0800

    【StockTrade:CBM】done

--- packages/bespoke/game/StockTrading/CBM/src/util.ts
+++ packages/bespoke/game/StockTrading/CBM/src/util.ts
@@ -7,3 +7,15 @@ export function getEnumKeys<E>(e: {}): Array<string> {
     }
     return keys
 }
+
+export function random(min:number, max:number):number {
+    return min + ~~(Math.random() * (max - min))
+}
+
+export function getBalanceIndex(countArr:number[]) {
+    let s = countArr.reduce((m, n) => m + n, 0) /2, i = 0
+    while (s>0) {
+        s -= countArr[i++]
+    }
+    return i-1
+}
