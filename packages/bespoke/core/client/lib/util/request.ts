6971a2f741c34cead89d99d70ae07274c6034a91
GuoKan <835111751@qq.com>
2019-06-10 17:00:10 +0800

    【bespoke-client-util】暴露buildUrl/get/post方法

--- packages/bespoke/core/client/lib/util/request.ts
+++ packages/bespoke/core/client/lib/util/request.ts
@@ -1,27 +1,50 @@
+import * as queryString from 'query-string'
 import {baseEnum, config} from 'bespoke-common'
 
-const baseFetchOption = {
-    credentials: 'include',
-    method: baseEnum.RequestMethod.GET,
-    headers: {'Content-Type': 'application/json; charset=utf-8'},
-    cache: 'default'
+export interface IHttpRes {
+    code: baseEnum.ResponseCode
 }
 
-function getCookie(key: string) {
-    return decodeURIComponent(document.cookie)
-        .split('; ')
-        .find(str => str.startsWith(`${key}=`))
-        .substring(key.length + 1)
-}
+export namespace Request {
+    const baseFetchOption = {
+        credentials: 'include',
+        method: baseEnum.RequestMethod.GET,
+        headers: {'Content-Type': 'application/json; charset=utf-8'},
+        cache: 'default'
+    }
 
-export async function request(url, method: baseEnum.RequestMethod = baseEnum.RequestMethod.GET, data = null): Promise<any> {
-    const option = {
-        ...baseFetchOption,
-        method,
-        ...(data ? {body: JSON.stringify({...data, _csrf: getCookie(config.cookieKey.csrf)})} : {})
-    } as RequestInit
-    const res = await fetch(url, option)
-    if (res.ok) {
-        return res.json()
+    async function request(url: string, method: baseEnum.RequestMethod = baseEnum.RequestMethod.GET, data = null): Promise<any> {
+        const option = {
+            ...baseFetchOption,
+            method,
+            ...(data ? {body: JSON.stringify({...data, _csrf: getCookie(config.cookieKey.csrf)})} : {})
+        } as RequestInit
+        const res = await fetch(url, option)
+        if (res.ok) {
+            return res.json()
+        }
+    }
+
+    function getCookie(key: string) {
+        return decodeURIComponent(document.cookie)
+            .split('; ')
+            .find(str => str.startsWith(`${key}=`))
+            .substring(key.length + 1)
+    }
+
+    export function buildUrl(namespace: string, pathFragment: string, params = {}, query = {}): string {
+        let path = `/${config.rootName}/${namespace}/${config.apiPrefix}${pathFragment}`
+        if (params) {
+            path = path.replace(/:([\w\d]+)/, (matchedParam, paramName) => params[paramName])
+        }
+        return `${path}?${queryString.stringify(query)}`
+    }
+
+    export async function get(namespace: string, path: string, params = {}, query = {}) {
+        return await request(buildUrl(namespace, path, params, query))
+    }
+
+    export async function post(namespace: string, path: string, params = {}, query = {}, data) {
+        return await request(buildUrl(namespace, path, params, query), baseEnum.RequestMethod.POST, data)
     }
 }
--- packages/bespoke/core/common/src/index.ts
+++ packages/bespoke/core/common/src/index.ts
@@ -1,7 +1,6 @@
-export {config} from './config'
-export * from './interface'
-export {FrameEmitter} from './FrameEmitter'
-export {Request} from './Request'
 import * as baseEnum from './baseEnum'
 
 export {baseEnum}
+export {config} from './config'
+export * from './interface'
+export {FrameEmitter} from './FrameEmitter'
