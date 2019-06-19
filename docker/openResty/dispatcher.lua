b88ca622381bec63feb054fe0c36420f5151dc5b
GuoKan <835111751@qq.com>
2019-06-19 17:36:23 +0800

    【openResty】缓存路由表

--- docker/openResty/dispatcher.lua
+++ docker/openResty/dispatcher.lua
@@ -6,17 +6,12 @@ ROUTE_TABLE = {
     wjxPhase = '127.0.0.1:4004',
     qqwjPhase = '127.0.0.1:4005',
 }
-GAME_SERVER_KEY_PREFIX = "bespokeGameServer:"
+BESPOKE_SERVER_KEY_PREFIX = "bespokeGameServer:"
 BESPOKE_PREFIX = 'bespoke'
+CACHE_TIME = 10
 
 if(ngx.var.request_method == 'HEAD')then
    ngx.exit(200)
-end
-red = require "resty.redis":new()
-red:set_timeout(1000)
-local _, err = red:connect(os.getenv("REDIS_IP"), os.getenv("REDIS_PORT"))
-if err then
-    ngx.log(ngx.ERR, 'Connect to redis failed : ' .. os.getenv("REDIS_IP") .. ':' .. os.getenv("REDIS_PORT"))
 end
 local pathWords = {}
 local serverAddress = ROUTE_TABLE.home
@@ -24,7 +19,22 @@ string.gsub(ngx.var.request_uri, "[^'..%/..']+", function(w)
     table.insert(pathWords, w)
 end)
 if pathWords[1] == BESPOKE_PREFIX then
-    serverAddress = red:get(GAME_SERVER_KEY_PREFIX .. pathWords[2])
+    local namespace = pathWords[2]
+    local bespokeServerCache = ngx.shared.bespokeServerCache
+    local bespokeServerAddress = bespokeServerCache:get(namespace)
+    ngx.log(ngx.ALERT, 'CachedServerAddress : ', bespokeServerAddress, '---', namespace)
+    if (bespokeServerAddress == nil)then
+        local red = require "resty.redis":new()
+        red:set_timeout(1000)
+        local _, err = red:connect(os.getenv("REDIS_IP"), os.getenv("REDIS_PORT"))
+        if err then
+            ngx.log(ngx.ERR, 'Connect to redis failed : ' .. os.getenv("REDIS_IP") .. ':' .. os.getenv("REDIS_PORT"))
+        end
+        bespokeServerAddress = red:get(BESPOKE_SERVER_KEY_PREFIX .. namespace)
+        red:set_keepalive(10000, 100)
+        bespokeServerCache:set(namespace, bespokeServerAddress, CACHE_TIME)
+    end
+    serverAddress = bespokeServerAddress
 else
     for k, v in pairs(ROUTE_TABLE) do
         if k == pathWords[1] then
@@ -34,4 +44,3 @@ else
 end
 ngx.log(ngx.ALERT, 'ServerAddress : ', serverAddress)
 ngx.var.target = serverAddress
-red:set_keepalive(10000, 100)
