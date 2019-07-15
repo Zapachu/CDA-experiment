a6dcd323d832fa6e3e84174212c449c5d812ae48
王禹衡 <193135213@qq.com>
2019-07-15 17:39:46 +0800

    情景问卷 fix

--- game/SceneSurvey/src/view/index.tsx
+++ game/SceneSurvey/src/view/index.tsx
@@ -3,8 +3,8 @@ import { Play } from "./Play";
 import { Play4Owner } from "./Play4Owner";
 import { Result4Owner } from "./Result4Owner";
 
-registerOnFramework("DecisionSurvey", {
-  localeNames: ["DecisionSurvey", "DecisionSurvey"],
+registerOnFramework("SceneSurvey", {
+  localeNames: ["SceneSurvey", "SceneSurvey"],
   Play,
   Play4Owner,
   Result4Owner
