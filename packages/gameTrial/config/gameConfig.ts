d469261b3db696fc99bc5bb16d0bfbfe8e8461e1
王禹衡 <193135213@qq.com>
2019-07-17 16:53:33 +0800

    gametrial 页面

--- packages/gameTrial/config/gameConfig.ts
+++ packages/gameTrial/config/gameConfig.ts
@@ -1,13 +1,17 @@
-import settings from '../settings'
+import settings from "../settings";
 
 // 实验config文件
 export default {
-    EgretDoubleAuction: {
-        img: `${settings.rootname}/static/sxpm_p.png`,
-        name: '双向拍卖'
-    },
-    ParallelApplication: {
-        img: `${settings.rootname}/static/pxzy_p.png`,
-        name: '平行志愿'
-    }
-}
+  EgretDoubleAuction: {
+    img: `${settings.rootname}/static/sxpm_p.png`,
+    name: "双向拍卖",
+    nameEn: "Double Auction",
+    coverImg: `${settings.rootname}/static/double_auction_cover.png`
+  },
+  ParallelApplication: {
+    img: `${settings.rootname}/static/pxzy_p.png`,
+    name: "平行志愿",
+    nameEn: "Parallel Application",
+    coverImg: `${settings.rootname}/static/parallel_application_cover.png`
+  }
+};
