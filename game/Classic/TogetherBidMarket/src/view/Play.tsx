83c8f2ef55a90e1eca4f090c316c3af431662638
王禹衡 <193135213@qq.com>
2019-07-26 13:45:55 +0800

    merge 集合竞价

--- game/Classic/TogetherBidMarket/src/view/Play.tsx
+++ game/Classic/TogetherBidMarket/src/view/Play.tsx
@@ -1,153 +1,230 @@
-import * as React from 'react'
-import * as style from './style.scss'
-import {Core} from '@bespoke/client'
-import {MaskLoading, Input, Label, Button, ButtonProps, Toast} from '@elf/component'
-import {ICreateParams, IGameState, IMoveParams, IPlayerState, IPushParams} from '../interface'
-import {MoveType, PushType, NEW_ROUND_TIMER, PlayerStatus} from '../config'
+import * as React from "react";
+import * as style from "./style.scss";
+import { Core } from "@bespoke/client";
+import {
+  MaskLoading,
+  Input,
+  Label,
+  Button,
+  ButtonProps,
+  Toast
+} from "@elf/component";
+import {
+  ICreateParams,
+  IGameState,
+  IMoveParams,
+  IPlayerState,
+  IPushParams,
+  MoveType,
+  PushType,
+  NEW_ROUND_TIMER,
+  PlayerStatus
+} from "../config";
 
 interface IPlayState {
-    price: string
-    loading: boolean
-    newRoundTimers: Array<number>
+  price: string;
+  num: string;
+  newRoundTimers: Array<number>;
 }
 
-export class Play extends Core.Play<ICreateParams, IGameState, IPlayerState, MoveType, PushType, IMoveParams, IPushParams, IPlayState> {
-    state = {
-        price: '',
-        loading: true,
-        newRoundTimers: []
+export class Play extends Core.Play<
+  ICreateParams,
+  IGameState,
+  IPlayerState,
+  MoveType,
+  PushType,
+  IMoveParams,
+  IPushParams,
+  IPlayState
+> {
+  state = {
+    price: "",
+    num: "",
+    newRoundTimers: []
+  };
+
+  async componentDidMount() {
+    const {
+      props: { frameEmitter }
+    } = this;
+    // frameEmitter.on(PushType.newRoundTimer, ({roundIndex, newRoundTimer}) => {
+    //     this.setState(state => {
+    //         const newRoundTimers = state.newRoundTimers.slice()
+    //         newRoundTimers[roundIndex] = newRoundTimer
+    //         return {newRoundTimers}
+    //     })
+    // })
+    frameEmitter.emit(MoveType.getPosition);
+  }
+
+  shout = () => {
+    const {
+      props: {
+        frameEmitter,
+        gameState: { groups },
+        playerState: { groupIndex }
+      },
+      state
+    } = this;
+    const price = Number(state.price);
+    const num = Number(state.num);
+    if (!price || !num) {
+      Toast.warn("输入的值无效");
+    } else {
+      frameEmitter.emit(MoveType.shout, {
+        price,
+        num: Math.floor(num),
+        roundIndex: groups[groupIndex].roundIndex
+      });
+      this.setState({ price: "", num: "" });
     }
+  };
 
-    async componentDidMount() {
-        const {props: {frameEmitter}} = this
-        frameEmitter.on(PushType.newRoundTimer, ({roundIndex, newRoundTimer}) => {
-            this.setState(state => {
-                const newRoundTimers = state.newRoundTimers.slice()
-                newRoundTimers[roundIndex] = newRoundTimer
-                return {newRoundTimers}
-            })
-        })
-        frameEmitter.emit(MoveType.getPosition)
-        this.setState({loading: false})
+  dynamicAction = () => {
+    const {
+      props: {
+        gameState: { groups },
+        playerState: { groupIndex, positionIndex, rounds: playerRounds }
+      },
+      state: { price, num }
+    } = this;
+    const { rounds: gameRounds, roundIndex } = groups[groupIndex],
+      { status } = playerRounds[roundIndex];
+    if (status === PlayerStatus.shouted) {
+      return <MaskLoading label="您已出价，请等待其他玩家..." />;
     }
-
-    setVal = (e) => this.setState({price: e.target.value})
-
-    shout = () => {
-        const {
-            props: {
-                frameEmitter,
-                gameState: {groups},
-                playerState: {groupIndex, privatePrices}
-            }, state
-        } = this
-        this.setState({price: ''})
-        const price = Number(state.price)
-        if (Number.isNaN(price) || price > privatePrices[groups[groupIndex].roundIndex]) {
-            Toast.warn('输入的值无效')
-        } else {
-            frameEmitter.emit(MoveType.shout, {price: +price})
-        }
+    if (status === PlayerStatus.result) {
+      return <MaskLoading label="所有轮次结束，等待老师结束实验..." />;
     }
+    return (
+      <div>
+        <li>
+          <Label label="输入您的价格" />
+          <Input
+            type="number"
+            value={price}
+            onChange={e => this.setState({ price: e.target.value })}
+          />
+        </li>
+        <li>
+          <Label label="输入您的配额数量" />
+          <Input
+            type="number"
+            value={num}
+            onChange={e => this.setState({ num: e.target.value })}
+          />
+        </li>
+        <li>
+          <Button
+            width={ButtonProps.Width.large}
+            label="出价"
+            onClick={this.shout.bind(this)}
+          />
+        </li>
+      </div>
+    );
+  };
 
-    dynamicAction = () => {
-        const {
-            props: {
-                gameState: {groups},
-                playerState: {groupIndex, positionIndex}
-            }, state: {price}
-        } = this
-        const {rounds, roundIndex} = groups[groupIndex],
-            {playerStatus} = rounds[roundIndex]
-        const playerState = playerStatus[positionIndex]
-        if (playerState === PlayerStatus.shouted) {
-            return <MaskLoading label='您已出价，请等待其他玩家...'/>
-        }
-        if (playerState === PlayerStatus.gameOver) {
-            return <MaskLoading label='所有轮次结束，等待老师结束实验...'/>
-        }
-        return <div>
-            <li>
-                <Label label='输入您的价格'/>
-                <Input type='number' value={price} onChange={this.setVal.bind(this)}/>
-            </li>
-            <li>
-                <Button width={ButtonProps.Width.large} label='出价' onClick={this.shout.bind(this)}/>
-            </li>
-        </div>
+  dynamicResult = () => {
+    const {
+      props: {
+        gameState: { groups },
+        playerState: { rounds: playerRounds, groupIndex, role }
+      }
+    } = this;
+    const { roundIndex, rounds: gameRounds } = groups[groupIndex];
+    const playerRound = playerRounds[roundIndex];
+    const gameRound = gameRounds[roundIndex];
+    if (roundIndex === 0) {
+      return null;
     }
+    return (
+      <table className={style.profits}>
+        <thead>
+          <tr>
+            <td>轮次</td>
+            <td>成交价格</td>
+            <td>{["购买股数", "出售股数"][role]}</td>
+            <td>心理价值</td>
+            <td>收益</td>
+          </tr>
+        </thead>
+        {Array(roundIndex)
+          .fill(null)
+          .map((_, i) => (
+            <tbody key={`tb${i}`}>
+              <tr>
+                <td>{i + 1}</td>
+                <td>{gameRound.strikePrice}</td>
+                <td>{playerRound.actualNum}</td>
+                <td>{playerRound.privateValue}</td>
+                <td>{playerRound.profit}</td>
+              </tr>
+            </tbody>
+          ))}
+      </table>
+    );
+  };
 
-    dynamicResult = () => {
-        const {props: {playerState: {profits, prices, privatePrices}}} = this
-        return <table className={style.profits}>
-            <thead>
-            <tr>
-                <td>轮次</td>
-                <td>心理价值</td>
-                <td>出价</td>
-                <td>收益</td>
-            </tr>
-            </thead>
-            {
-                prices.filter((p) => p !== 0).map((v, i) =>
-                    <tbody key={`tb${i}`}>
-                    <tr>
-                        <td>{i + 1}</td>
-                        <td>{privatePrices[i]}</td>
-                        <td>{v}</td>
-                        <td>{profits[i]}</td>
-                    </tr>
-                    </tbody>
-                )
-            }
-        </table>
+  render() {
+    const {
+      props: {
+        game: {
+          params: { groupSize, rounds }
+        },
+        gameState: { groups },
+        playerState: { role, groupIndex, rounds: playerRounds }
+      },
+      state: { newRoundTimers }
+    } = this;
+    if (groupIndex === undefined) {
+      return <MaskLoading label="正在匹配玩家..." />;
     }
-
-    render() {
-        const {
-            props: {
-                game: {params: {groupSize}},
-                gameState: {groups},
-                playerState: {role, groupIndex, privatePrices}
-            }, state: {loading, newRoundTimers}
-        } = this
-        if (loading) {
-            return <MaskLoading label='加载中...'/>
-        }
-        if (groupIndex === undefined) {
-            return <MaskLoading label='正在匹配玩家...'/>
-        }
-        const {rounds, roundIndex} = groups[groupIndex],
-            newRoundTimer = newRoundTimers[roundIndex]
-        return <section className={style.play}>
-            <div className={style.title}>集合竞价市场</div>
-            {newRoundTimer ? <div>
+    const { rounds: gameRounds, roundIndex } = groups[groupIndex];
+    // newRoundTimer = newRoundTimers[roundIndex]
+    return (
+      <section className={style.play}>
+        <div className={style.title}>集合竞价市场</div>
+        {/* {newRoundTimer ? <div>
                 <div>本轮结束剩余时间</div>
                 <div className={style.highlight}>{NEW_ROUND_TIMER - newRoundTimer}</div>
-            </div> : null}
-            <div className={style.line}>
-                <div>游戏总人数</div>
-                <div className={style.highlight}>{groupSize}</div>
-            </div>
-            <div className={style.line}>
-                <div>游戏总轮数</div>
-                <div className={style.highlight}>{rounds.length}</div>
-            </div>
-            <div className={style.line}>
-                <div>正在进行轮次</div>
-                <div className={style.highlight}>{roundIndex + 1} </div>
-            </div>
-            <div className={style.line}>
-                <div>您的角色</div>
-                <div className={style.highlight}>{['买家', '卖家'][role]}</div>
-            </div>
-            <div className={style.line}>
-                <div>物品对于您的心理价值</div>
-                <div className={style.highlight}>{privatePrices[groups[groupIndex].roundIndex]}</div>
-            </div>
-            {this.dynamicAction()}
-            {this.dynamicResult()}
-        </section>
-    }
-
+            </div> : null} */}
+        <div className={style.line}>
+          <div>游戏总人数</div>
+          <div className={style.highlight}>{groupSize}</div>
+        </div>
+        <div className={style.line}>
+          <div>游戏总轮数</div>
+          <div className={style.highlight}>{rounds}</div>
+        </div>
+        <div className={style.line}>
+          <div>正在进行轮次</div>
+          <div className={style.highlight}>{roundIndex + 1} </div>
+        </div>
+        <div className={style.line}>
+          <div>您的角色</div>
+          <div className={style.highlight}>{["买家", "卖家"][role]}</div>
+        </div>
+        <div className={style.line}>
+          <div>物品对于您的心理价值</div>
+          <div className={style.highlight}>
+            {playerRounds[roundIndex].privateValue}
+          </div>
+        </div>
+        <div className={style.line}>
+          <div>{["您的初始资金", "您的股票配额"][role]}</div>
+          <div className={style.highlight}>
+            {
+              [
+                playerRounds[roundIndex].startingPrice,
+                playerRounds[roundIndex].startingQuota
+              ][role]
+            }
+          </div>
+        </div>
+        {this.dynamicAction()}
+        {this.dynamicResult()}
+      </section>
+    );
+  }
 }
