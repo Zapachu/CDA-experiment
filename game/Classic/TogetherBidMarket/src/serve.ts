a124acd90c1078353fb9c4e3c8ddb5b58aa3c69e
GuoKan <835111751@qq.com>
2019-08-22 15:42:07 +0800

    ...

--- game/Classic/TogetherBidMarket/src/serve.ts
+++
@@ -1,6 +0,0 @@
-import {resolve} from 'path'
-import {Server} from '@bespoke/server'
-import Controller from './Controller'
-import {namespace} from './config'
-
-Server.start(namespace, Controller, resolve(__dirname, '../dist'))
