5fa8a4221ab56ca71fafffa285def972bb03cb93
GuoKan <835111751@qq.com>
2019-09-09 22:16:30 +0800

    ...

--- packages/whuipo/backend/rpc.ts
+++ packages/whuipo/backend/rpc.ts
@@ -7,15 +7,28 @@ import request from 'request-promise-native';
 import {RedisCall, Trial} from '@elf/protocol';
 import {Log} from '@elf/util';
 
-export async function sendBackData(body) {
+export async function sendBackData(iLabXUserName: string, childProjectTitle:string) {
+    //TODO 字段待完善
+    const score = ~~(Math.random() * 30 + 70), timeUsed = (Math.random() * 10 + 2);
+    const data = {
+        username: iLabXUserName,
+        projectTitle: '金融市场与算法交易虚拟仿真教学平台',
+        childProjectTitle,
+        status: '1',
+        score,
+        startDate: Date.now() - ~~(timeUsed * 6e4 - Math.random() * 1e4),
+        endDate: Date.now(),
+        timeUsed: ~~timeUsed,
+        issuerId: '100400'
+    };
     try {
-        const token = XJWT.encode(XJWTType.SYS, body);
+        const token = XJWT.encode(XJWTType.SYS, data);
         const response = await request({
             url: `${setting.iLabXGateWay}/project/log/upload?xjwt=${encodeURIComponent(token)}`
         });
         Log.d(response);
     } catch (e) {
-        Log.e('Failed to post data', body);
+        Log.e('Failed to post data', data);
     }
 }
 
@@ -24,9 +37,9 @@ export async function sendUserStatus(userName: string) {
         username: userName,
         issuerId: setting.issuerId
     });
-    const xjwt = encodeURIComponent(_xjwt)
+    const xjwt = encodeURIComponent(_xjwt);
     const response = await request({
-        url:`${setting.iLabXGateWay}/third/api/test/result/upload?xjwt=${xjwt}`
+        url: `${setting.iLabXGateWay}/third/api/test/result/upload?xjwt=${xjwt}`
     });
     Log.d(response);
 }
@@ -40,7 +53,12 @@ export function runRPC() {
             const user = await User.findById(uid);
             if (user) {
                 await sendUserStatus(user.iLabXUserName);
-                await sendBackData({timestamp: Date.now()});
+                await sendBackData(user.iLabXUserName, {
+                    [Phase.IPO]:'中位数定价/荷兰式拍卖/第一价格密封拍卖',
+                    [Phase.OpenAuction]:'公开竞价拍卖',
+                    [Phase.TBM]:'集合竞价',
+                    [Phase.CBM]:'连续竞价/融资融券',
+                }[phase]);
                 user.score += 10;
                 await user.save();
                 await RedisTools.setUserGameData(uid, phase, {
