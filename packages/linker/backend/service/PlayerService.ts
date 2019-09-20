1cccd2699898d23078f250cf3f1885f4ee66625a
GuoKan <835111751@qq.com>
2019-09-20 11:27:33 +0800

    【linker】补充玩家orgCode

--- packages/linker/backend/service/PlayerService.ts
+++ packages/linker/backend/service/PlayerService.ts
@@ -1,19 +1,19 @@
-import {PlayerModel} from '../model'
-import {TApiPlayers} from 'linker-share'
+import {GameModel, PlayerModel, UserModel} from '../model';
 
 export class PlayerService {
     static async savePlayer(gameId: string, userId: string): Promise<string> {
-        const player = await new PlayerModel({gameId, userId}).save()
-        return player.id
+        const user = await UserModel.findById(userId);
+        if (!user.orgCode) {
+            const {owner} = await GameModel.findById(gameId),
+                {orgCode, orgName} = await UserModel.findById(owner) as any;
+            await UserModel.findByIdAndUpdate(userId, {orgCode, orgName});
+        }
+        const player = await new PlayerModel({gameId, userId}).save();
+        return player.id;
     }
 
     static async findPlayerId(gameId: string, userId: string): Promise<string> {
-        const player = await PlayerModel.findOne({gameId, userId})
-        return player ? player.id : null
-    }
-
-    static async getPlayers(gameId: string):Promise<TApiPlayers> {
-        const players = await PlayerModel.find({gameId}).populate('userId', 'name') as any
-        return players.map(({id, userId})=>({playerId:id, userId:userId.id, name:userId.name}))
+        const player = await PlayerModel.findOne({gameId, userId});
+        return player ? player.id : null;
     }
 }
