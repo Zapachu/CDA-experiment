67862073db9d18358c6f487acff0358443415c90
gxqn <627341160@qq.com>
2019-10-11 16:58:23 +0800

    regC2 类型校验

--- game/Custom/ShanghaiNY/V4/src/view/Play/regC2Group.tsx
+++ game/Custom/ShanghaiNY/V4/src/view/Play/regC2Group.tsx
@@ -10,10 +10,10 @@ interface Args{
     mode: Mode,
 }
 
-export function regC2({playersPerGroup,c1,c2,mode}:Args) {
+export function regC2({playersPerGroup,c1,c2,mode}:Args):boolean {
     if(mode === Mode.BR) {
         if(c1 === 1) {
-            let flag:boolean
+            let flag:boolean = false
             c2.forEach((val,index) => {
                 if(index > 0 && val === undefined) {
                     flag = true
@@ -22,12 +22,18 @@ export function regC2({playersPerGroup,c1,c2,mode}:Args) {
             return (!c1 || flag || c2.length !== playersPerGroup + 1)
         }else if(c1 === 2) {
             return (!c1 || c2.includes(undefined) || c2.length !== playersPerGroup)
+        }else {
+            return true
         }
     }else if(mode === Mode.HR || mode === Mode.LR) {
         if(c1 === 1 || c1 === 2) {
             return (!c1 || c2.includes(undefined) || c2.length !== playersPerGroup + 1)
         }else if(c1 === -1){
             return (!c1 || c2.includes(undefined) || c2.length !== playersPerGroup)
+        }else {
+            return true
         }
+    }else {
+        return true
     }
 }
