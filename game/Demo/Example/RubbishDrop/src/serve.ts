1efc81d17206fbeec35829dca7552be80ab1b8b9
gxqn <627341160@qq.com>
2019-10-18 11:52:16 +0800

    垃圾分类

---
+++ game/Demo/Example/RubbishDrop/src/serve.ts
@@ -0,0 +1,6 @@
+import {resolve} from 'path';
+import {namespace} from './config';
+import {Server} from '@bespoke/server';
+import {Logic} from './Logic';
+
+Server.start(namespace, Logic, resolve(__dirname, '../dist'));
