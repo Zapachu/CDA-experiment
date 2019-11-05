614d3da2f220cf984a079da8cf98860bb0637272
GuoKan <835111751@qq.com>
2019-11-05 11:59:30 +0800

    【GameTrail】...

--- game/Trial/GarbageSorting/V2/src/serve.ts
+++ game/Trial/GarbageSorting/V2/src/serve.ts
@@ -1,8 +1,24 @@
 import {resolve} from 'path';
-import {Server} from '@bespoke/server';
+import {gameId2PlayUrl, RedisCall, Server} from '@bespoke/server';
+import {ICreateParams, namespace} from './config';
+import {Trial} from '@elf/protocol';
 import {RobotServer} from '@bespoke/robot';
 import {Logic, Robot} from './Logic';
-import {namespace} from './config';
 
 Server.start(namespace, Logic, resolve(__dirname, '../dist'));
 RobotServer.start(namespace, Robot);
+
+RedisCall.handle<Trial.Create.IReq, Trial.Create.IRes>(
+    Trial.Create.name(namespace),
+    async () => {
+        const gameId = await Server.newGame<ICreateParams>({
+            title: `${namespace}:${new Date().toUTCString()}`,
+            params: {
+                groupSize: 10
+            }
+        });
+        return {playUrl: gameId2PlayUrl(gameId)};
+    }
+);
+
+
