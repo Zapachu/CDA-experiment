9fe9b21380588b0fc9c00a810caded5c6248239c
GuoKan <835111751@qq.com>
2019-11-05 11:08:02 +0800

    【GameTrail】...

---
+++ pm2scripts/trial.config.js
@@ -0,0 +1,36 @@
+const apps = [
+    {
+        script: "../packages/gameTrial/build/app.js",
+        name: "gameTrial",
+    },
+    {
+        script: "../game/WithEgret/DoubleAuction/build/serve.js",
+        name: "EgretDoubleAuction",
+    },
+    {
+        script: "../game/Trial/ParallelApplication/build/serve.js",
+        name: "ParallelApplication",
+    },
+    {
+        script: "../game/Trial/GarbageSorting/build/serve.js",
+        name: "GarbageSorting",
+    },
+    {
+        script: "../game/Trial/GarbageSortingV2/build/serve.js",
+        name: "GarbageSortingV2",
+    }
+];
+
+module.exports = {
+    apps: apps.map(app => ({
+        ...app,
+        autorestart: true,
+        watch: true,
+        max_memory_restart: '1G',
+        log_date_format: "YYYY-MM-DD HH:mm Z",
+        env: {
+            NODE_ENV: "production",
+            BESPOKE_WITH_PROXY: "true"
+        }
+    }))
+};
