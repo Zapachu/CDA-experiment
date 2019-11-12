e61e02d85801ac11b22cbaeeb85f861fb300895f
GuoKan <835111751@qq.com>
2019-11-12 18:45:44 +0800

    【CDA】GDRobot(doing)

--- game/CDA/ContinuousDoubleAuction/src/Robot.ts
+++ game/CDA/ContinuousDoubleAuction/src/Robot.ts
@@ -1,11 +1,11 @@
-import {Model, redisClient} from '@bespoke/server';
+import {Log, Model, redisClient} from '@bespoke/server';
 import {BaseRobot} from '@bespoke/robot';
 import {
     AdjustDirection,
     DBKey,
     IDENTITY,
     MarketStage,
-    MoveType,
+    MoveType, orderNumberLimit,
     phaseNames,
     PushType,
     ReactionType,
@@ -15,6 +15,7 @@ import {
     ROLE,
     ShoutResult
 } from './config';
+import {pointPair2Curve} from './util';
 import {CreateParams, GameState, ICreateParams, IGameState, IMoveParams, IPlayerState, IPushParams} from './interface';
 import dateFormat = require('dateformat');
 
@@ -33,6 +34,7 @@ export default class Robot extends BaseRobot<ICreateParams, IGameState, IPlayerS
                     new ZipRobot(this).init();
                     break;
                 case IDENTITY.GDRobot:
+                    new GDRobot(this).init();
                     break;
             }
         });
@@ -77,12 +79,12 @@ class CDARobot {
     }
 
     get unitPrice(): number {
-        try {
-            const {gameState: {gamePhaseIndex}, playerState: {unitLists}} = this;
-            return +(unitLists[gamePhaseIndex].split(' ')[this.unitIndex]);
-        } catch (e) {
-            return 0;
-        }
+        return this.unitPrices[this.unitIndex] || 0;
+    }
+
+    get unitPrices(): Array<number> {
+        const {gameState: {gamePhaseIndex}, playerState: {unitLists}} = this;
+        return unitLists[gamePhaseIndex].split(' ').map(p => +p);
     }
 
     get position(): CreateParams.Phase.Params.IPosition {
@@ -93,12 +95,42 @@ class CDARobot {
         return position;
     }
 
+    getPosition(index: number): CreateParams.Phase.Params.IPosition {
+        const {positions} = this.game.params.phases[0].params;
+        return positions[index];
+
+    }
+
     formatPrice(price: number) {
         return Math.round(price);
     }
 
     async init(): Promise<CDARobot> {
         return this;
+    }
+
+    buildRobotSubmitLog(seq: number, price: number): Partial<RobotSubmitLog> {
+        const {unitIndex, unitPrice, gamePhaseState: {buyOrderIds, sellOrderIds}, orderDict} = this;
+        return {
+            seq,
+            playerSeq: this.playerState.positionIndex + 1,
+            unitIndex,
+            role: ROLE[this.position.role],
+            ValueCost: unitPrice,
+            price,
+            buyOrders: buyOrderIds.map(id => orderDict[id].price).join(','),
+            sellOrders: sellOrderIds.map(id => orderDict[id].price).join(','),
+            timestamp: dateFormat(Date.now(), 'HH:MM:ss:l')
+        };
+    }
+
+    wouldBeRejected(price: number): [boolean, number] {
+        const {gamePhaseState: {buyOrderIds, sellOrderIds}, orderDict} = this;
+        const [wouldBeRejected = false, rejectPrice] = {
+            [ROLE.Seller]: sellOrderIds[0] ? [price >= orderDict[sellOrderIds[0]].price, orderDict[sellOrderIds[0]].price] : [],
+            [ROLE.Buyer]: buyOrderIds[0] ? [price <= orderDict[buyOrderIds[0]].price, orderDict[buyOrderIds[0]].price] : []
+        }[this.position.role];
+        return [wouldBeRejected, rejectPrice];
     }
 }
 
@@ -240,30 +272,150 @@ class ZipRobot extends CDARobot {
         const {
             zipFreeField: {calcPrice},
             gameState: {gamePhaseIndex},
-            gamePhaseState: {buyOrderIds, sellOrderIds},
-            orderDict, unitIndex, unitPrice
+            unitIndex
         } = this;
-        const [wouldBeRejected = false, rejectPrice] = {
-            [ROLE.Seller]: sellOrderIds[0] ? [calcPrice >= orderDict[sellOrderIds[0]].price, orderDict[sellOrderIds[0]].price] : [],
-            [ROLE.Buyer]: buyOrderIds[0] ? [calcPrice <= orderDict[buyOrderIds[0]].price, orderDict[buyOrderIds[0]].price] : []
-        }[this.position.role];
+        const [wouldBeRejected, rejectPrice] = this.wouldBeRejected(calcPrice);
         if (wouldBeRejected) {
             this.zipFreeField.calcPrice = calcPrice;
             this.adjustProfitRate(AdjustDirection.lower, rejectPrice);
             return;
         }
         this.zipFreeField.u = calcPrice / this.unitPrice - 1;
-        const data: Partial<RobotSubmitLog> = {
-            seq,
-            playerSeq: this.playerState.positionIndex + 1,
-            unitIndex,
-            role: ROLE[this.position.role],
-            ValueCost: unitPrice,
+        const data = this.buildRobotSubmitLog(seq, calcPrice);
+        this.frameEmitter.emit(MoveType.submitOrder, {
             price: calcPrice,
-            buyOrders: buyOrderIds.map(id => orderDict[id].price).join(','),
-            sellOrders: sellOrderIds.map(id => orderDict[id].price).join(','),
-            timestamp: dateFormat(Date.now(), 'HH:MM:ss:l')
-        };
+            unitIndex,
+            gamePhaseIndex
+        }, async (shoutResult: ShoutResult, marketBuyOrders, marketSellOrders) => {
+            await new Model.FreeStyleModel({
+                game: this.game.id,
+                key: DBKey.robotSubmitLog,
+                data: {...data, shoutResult, marketBuyOrders, marketSellOrders}
+            }).save();
+        });
+    }
+}
+
+class GDRobot extends CDARobot {
+    alpha = .95;
+    beta = 400;
+    calcPrice = 0;
+
+    async init(): Promise<this> {
+        await super.init();
+        global.setTimeout(()=>this.sleepLoop(), Math.random() * 5e3)
+        return this;
+    }
+
+    sleepLoop() {
+        const playing = this.game.params.phases[this.gameState.gamePhaseIndex].templateName === phaseNames.mainGame &&
+            this.gamePhaseState.marketStage === MarketStage.trading && this.unitPrice;
+        if (!playing) {
+            return;
+        }
+        this.calc();
+        const {alpha, beta, calcPrice} = this,
+            sleepTime = beta * (1 - alpha) * calcPrice;
+        setTimeout(() => {
+            redisClient.incr(RedisKey.robotActionSeq(this.game.id)).then(seq => this.submitOrder(seq));
+            this.sleepLoop();
+        }, sleepTime);
+    }
+
+    getM() {
+        let M = 0;
+        for (let price of this.unitPrices) {
+            price > M ? M = price : null;
+        }
+        return Math.pow(2, 2 + ~~Math.log2(M));
+    }
+
+    getExpectationCurve(): Array<{ from: number, to: number, curve: (price: number) => number }> {
+        const {gameState: {orders, gamePhaseIndex}, gamePhaseState: {trades}} = this,
+            recentTrades = trades.slice(-5),
+            recentReqOrders = recentTrades.map(({reqId}) => reqId),
+            recentResOrders = recentTrades.map(({resId}) => resId),
+            M = this.getM();
+        let anchorPoints: Array<{
+            price: number
+            p: number
+        }> = this.position.role === ROLE.Seller ?
+            [{price: 0, p: 1}, {price: M, p: 0}] :
+            [{price: 0, p: 0}, {price: M, p: 1}];
+        const markedOrderId = recentResOrders[0] || orderNumberLimit * gamePhaseIndex,
+            historyOrder = orders.filter(({id}) => (id > markedOrderId) && !recentResOrders.includes(id));
+        historyOrder.map(({price}) => {
+            if(anchorPoints.some(p=>p.price === price)){
+                return
+            }
+            let TA = 0, B = 0, RA = 0, TB = 0, A = 0, RB = 0;
+            for (let order of historyOrder) {
+                const isSeller = this.getPosition(order.positionIndex);
+                if (order.price >= price) {
+                    isSeller ? recentReqOrders.includes(order.id) ? TA++ : RA++ : B++;
+                }
+                if (order.price <= price) {
+                    isSeller ? A++ : recentReqOrders.includes(order.id) ? TB++ : RB++;
+                }
+            }
+            const p = this.position.role === ROLE.Seller ? (TA + B) / (TA + B + RA) : (TB + A) / (TB + A + RB);
+            anchorPoints.push({price, p});
+        });
+        anchorPoints = anchorPoints.sort((p1, p2) => p1.price - p2.price);
+        Log.d(this.position.role === ROLE.Seller, this.unitPrice, anchorPoints)
+        return anchorPoints.slice(1).map((p2, i) => {
+            const p1 = anchorPoints[i];
+            return {
+                from: p1.price,
+                to: p2.price,
+                curve: pointPair2Curve(p1, p2)
+            };
+        });
+    }
+
+    getCurvesTopPoint(curves: Array<{ from: number, to: number, curve: (price: number) => number }>, f: number, t: number, coefficient: (price: number) => number): {
+        price: number,
+        curveIndex: number
+    } {
+        let curveIndex = 0, maxE = 0, maxEPrice = f;
+        for (let price = f+1; price < t; price++) {
+            const {to, curve} = curves[curveIndex];
+            const e = coefficient(price) * curve(price);
+            if (e > maxE) {
+                maxE = e;
+                maxEPrice = price;
+            }
+            if (price === to) {
+                curveIndex++;
+            }
+        }
+        return {price: maxEPrice, curveIndex};
+    }
+
+    calc() {
+        const {gamePhaseState: {sellOrderIds, buyOrderIds}, orderDict} = this;
+        const curves = this.getExpectationCurve();
+        let f: number = null, t: number = null, profitSign = 1;
+        if (this.position.role === ROLE.Seller) {
+            f = this.unitPrice;
+            t = sellOrderIds[0] ? orderDict[sellOrderIds[0]].price : curves[curves.length - 1].to;
+        } else {
+            profitSign = -1;
+            f = buyOrderIds[0] ? orderDict[buyOrderIds[0]].price : curves[0].from;
+            t = this.unitPrice;
+        }
+        this.calcPrice = this.getCurvesTopPoint(curves, f, t, price => (price - this.unitPrice) * profitSign).price;
+    }
+
+
+    submitOrder(seq: number): void {
+        const {gameState: {gamePhaseIndex}, calcPrice, unitIndex} = this;
+        const [wouldBeRejected = false, rejectPrice] = this.wouldBeRejected(this.calcPrice);
+        if (wouldBeRejected) {
+            Log.i('Reject', rejectPrice);
+            return;
+        }
+        const data = this.buildRobotSubmitLog(seq, this.calcPrice);
         this.frameEmitter.emit(MoveType.submitOrder, {
             price: calcPrice,
             unitIndex,
--- game/CDA/ContinuousDoubleAuction/src/util.ts
+++ game/CDA/ContinuousDoubleAuction/src/util.ts
@@ -1,9 +1,17 @@
-export function getEnumKeys<E>(e:{}):Array<string> {
-    const keys:Array<string> = []
-    for(let key in e){
-        if(typeof e[key] === "number"){
-            keys.push(key)
+export function getEnumKeys<E>(e: {}): Array<string> {
+    const keys: Array<string> = [];
+    for (let key in e) {
+        if (typeof e[key] === 'number') {
+            keys.push(key);
         }
     }
-    return keys
+    return keys;
+}
+
+export function pointPair2Curve(p1: { price: number, p: number }, p2: { price: number, p: number }): (price: number) => number {
+    const a = -2 * (p1.p - p2.p) / Math.pow(p1.price - p2.price, 3),
+        b = -1.5 * (p1.price + p2.price) * a,
+        c = 3 * p1.price * p2.price * a,
+        d = p1.p - a * Math.pow(p1.price, 3) - b * Math.pow(p1.price, 2) - c * p1.price;
+    return (price: number) => a * Math.pow(price, 3) + b * Math.pow(price, 2) + c * price + d;
 }
